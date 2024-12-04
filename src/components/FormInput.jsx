3c3c70af076a5378767fb2add520cde893ff5df1
yungflashh <streaktv8@gmail.com>
2024-12-04 23:22:35 +0100

    fixed the error in the contact form

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import '../styles/FormInput.css'; 
+import {this.props.first} from "react"
 
 const FormInput = ({ label, type, name }) => {
   const [value, setValue] = useState('');
