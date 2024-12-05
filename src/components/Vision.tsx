2ca9489db0db254f47f5ccffc6f18af0f3ae3d61
yungflashh <streaktv8@gmail.com>
2024-12-06 00:03:18 +0100

    added vision statement

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Vision = () => {
   return (
-    <div>Vision</div>
+    <div>
+        <div className="visionCOntainer">
+                <div className="vision-text">
+                    <h2>VISION STATEMENT</h2>
+                    <p>To close the gap between recruiters and untapped talent, streamlining the hiring process so that both recruiters and candidates are equally visible, valued, and able to connect effortlessly.</p>
+                </div>
+
+                <div className="visionImg">
+                    
+                </div>
+
+        </div>
+    </div>
   )
 }
 
