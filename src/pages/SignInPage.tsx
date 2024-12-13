63c8e4b86dd87aebbd6d23dd59e10b7067e3bd87
yungflashh <streaktv8@gmail.com>
2024-12-13 23:50:26 +0100

    started with SignIn page

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import FormInput from '../components/FormInput'
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
+    <div>
+        
+        <div className="signIn-container">
+            <h2>Sign In</h2>
+
+           
+            <FormInput label='Enter Email Address' type='text' name='email' />
+        </div>
+    </div>
   )
 }
 
